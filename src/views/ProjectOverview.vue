2513d639752d9e42d290b78cbd5a36222e1cf31e
luca-macedone <luca.macedone@outlook.it>
2023-06-14 17:30:45 +0200

    minor fix

--- src/views/ProjectOverview.vue
+++ src/views/ProjectOverview.vue
@@ -20,7 +20,7 @@ export default {
         <!-- {{ store.project_overview.title }} -->
         <div class="p-5 mb-4 bg-transparent rounded-0 ms-text-light">
             <div class="container-fluid py-5">
-                <h1 class="display-5 fw-bold text-shadow">{{ store.project_overview }}</h1>
+                <h1 class="display-5 fw-bold text-shadow">{{ store.project_overview.title }}</h1>
                 <!-- <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in
                     previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to
                     your liking.</p> -->
