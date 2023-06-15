24eaf1627919aa3b69da9ffaf852724e21967b30
luca-macedone <luca.macedone@outlook.it>
2023-06-15 11:57:12 +0200

    add placeholder cards

--- src/store.js
+++ src/store.js
@@ -5,4 +5,5 @@ import { reactive } from "vue";
 export const store = reactive({
     base_url: 'http://127.0.0.1:8000/',
     projects_api: 'api/projects/',
+    project_placeholders: 6,
 });
--- src/views/ProjectList.vue
+++ src/views/ProjectList.vue
@@ -20,7 +20,7 @@ export default {
                 .get(url)
                 .then(result => {
                     this.projects_list = result.data.projects.data;
-                    console.log(this.projects_list)
+                    // console.log(this.projects_list)
                 })
                 .catch(error => {
                     console.error(error);
@@ -40,6 +40,30 @@ export default {
                 <h1 class="ms-text-light py-3">My projects</h1>
                 <div class="row g-4" v-if="projects_list">
                     <ProjectCard :data="project" v-for="project in projects_list" />
+                </div>
+
+                <div v-else>
+                    <div class="row g-4 py-3">
+                        <div class="col-12 col-md-6 col-lg-4" v-for="placeholder in store.project_placeholders">
+                            <div class="card h-100 rounded-0 ms-border-secondary ms-bg-light ms-text-dark shadow">
+                                <img src="..." class="project_image rounded-0 border-0" height="300" alt="">
+                                <div class="card-body card-img-overlay rounded-0">
+                                    <div
+                                        class="h-100 d-flex flex-column justify-content-between align-items-start placeholder-glow">
+                                        <div class="card-title fs-4 text-uppercase placeholder"></div>
+                                        <div class="d-flex flex-column align-items-start gap-3">
+                                            <div class="d-flex justify-content-start align-items-center gap-2 flex-wrap">
+                                                <span
+                                                    class="badge rounded-pill ms-bg-secondary px-3 py-2 p-2 placeholder"></span>
+                                            </div>
+                                            <div class="placeholder"></div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
