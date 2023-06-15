eca6d40c1d7d6862ef0d32ac87dd21ae0217d047
luca-macedone <luca.macedone@outlook.it>
2023-06-15 17:26:48 +0200

    add pagination

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import { router } from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faX, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-library.add(faMagnifyingGlass, faX)
+library.add(faMagnifyingGlass, faX, faChevronLeft, faChevronRight)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
--- src/views/ProjectList.vue
+++ src/views/ProjectList.vue
@@ -12,6 +12,14 @@ export default {
             store,
             projects_url: store.base_url + store.projects_api,
             projects_list: null,
+            current_page__number: 0,
+            // last_page__number: 0,
+            first_page__url: null,
+            last_page__url: null,
+            prev_page__url: null,
+            next_page__url: null,
+            total_projects: 0,
+
         }
     },
     methods: {
@@ -20,6 +28,18 @@ export default {
                 .get(url)
                 .then(result => {
                     this.projects_list = result.data.projects.data;
+                    // this.current_page__number = result.data.project.current_page;
+                    // this.last_page__number = result.data.project.current_page;
+                    this.first_page__url = result.data.projects.first_page_url;
+                    // console.log(this.first_page__url);
+                    this.last_page__url = result.data.projects.last_page_url;
+                    // console.log(this.last_page__url);
+                    this.prev_page__url = result.data.projects.prev_page_url;
+                    // console.log(this.prev_page__url);
+                    this.next_page__url = result.data.projects.next_page_url;
+                    // console.log(this.next_page__url);
+                    this.total_projects = result.data.projects.total;
+                    this.current_page__number = result.data.projects.current_page;
                     // console.log(this.projects_list)
                 })
                 .catch(error => {
@@ -65,6 +85,36 @@ export default {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="w-100 d-flex justify-content-center align-items-center" v-if="next_page__url || prev_page__url">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination ms-pagination">
+                        <li class="page-item ms-pagination-item" v-if="prev_page__url">
+                            <a class="page-link ms-pagination-link rounded-0" @click="getProjects(prev_page__url)"
+                                aria-label="Prev">
+                                <!-- <span aria-hidden="true">&laquo;</span> -->
+                                <font-awesome-icon aria-hidden="true" icon="fa-solid fa-chevron-left" />
+                            </a>
+                        </li>
+                        <li class="page-item ms-pagination-item" v-if="prev_page__url"><a
+                                class="page-link ms-pagination-link rounded-0" @click="getProjects(prev_page__url)">{{
+                                    current_page__number - 1 }}</a></li>
+                        <li class="page-item ms-pagination-item"><a class="page-link ms-pagination-link rounded-0"
+                                @click="getProjects(`http://127.0.0.1:8000:/api/projects/?page=${current_page__number}`)">{{
+                                    current_page__number }}</a>
+                        </li>
+                        <li class="page-item ms-pagination-item" v-if="next_page__url"><a
+                                class="page-link ms-pagination-link rounded-0" @click="getProjects(next_page__url)">{{
+                                    current_page__number + 1 }}</a></li>
+                        <li class="page-item ms-pagination-item" v-if="next_page__url">
+                            <a class="page-link ms-pagination-link rounded-0" @click="getProjects(next_page__url)"
+                                aria-label="Next">
+                                <!-- <span aria-hidden="true">&raquo;</span> -->
+                                <font-awesome-icon aria-hidden="true" icon="fa-solid fa-chevron-right" />
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </section>
     </main>
