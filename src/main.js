79ae25395754b717d66a49a42c1b0f344a4ed936
luca-macedone <luca.macedone@outlook.it>
2023-06-15 15:58:37 +0200

    add font awesome

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faMagnifyingGlass, faX } from '@fortawesome/free-solid-svg-icons'
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-createApp(App).use(router).mount('#app')
+library.add(faMagnifyingGlass, faX)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
