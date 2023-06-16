8de27e0ee0fa15cbcdc59f67398abd1e7041bd53
luca-macedone <luca.macedone@outlook.it>
2023-06-16 18:19:12 +0200

    minor fix

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="ms-bg-primary vh-100 vw-100 d-flex flex-column justify-content-center align-items-center gap-5">
         <h1 class="ms-text-light">Error 404 | Not Found</h1>
-        <RouterLink class="p-3 text-decoration-none back-btn" :to="{ name: 'projects' }">Back
-            to
-            projects
+        <RouterLink class="p-3 text-decoration-none back-btn" :to="{ name: 'projects' }">
+            Back to projects
         </RouterLink>
     </div>
 </template>
