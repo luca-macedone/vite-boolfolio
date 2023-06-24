85fafd2c3f811bbfcc79d4aa1024c0cc6df8665a
luca-macedone <luca.macedone@outlook.it>
2023-06-24 10:53:11 +0200

    update footer content

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,20 +8,23 @@ export default {
     <footer class="ms-bg-secondary py-5 ms-text-light">
         <div class="container">
             <div class="row">
-                <ul class="list-unstyled">
+                <!-- <ul class="list-unstyled">
                     <li class="">
                         <a class="nav-link" href="#">some text</a>
                     </li>
-                    <li class="">
-                        <a class="nav-link" href="#">some text</a>
-                    </li>
-                    <li class="">
-                        <a class="nav-link" href="#">some text</a>
-                    </li>
-                    <li class="">
-                        <a class="nav-link" href="#">some text</a>
-                    </li>
-                </ul>
+                </ul> -->
+                <div class="col-12 col-lg-6">
+
+                </div>
+                <div class="col-12 col-lg-6">
+
+                </div>
+                <div class="col-12">
+                    <h5 class="d-flex justify-content-center align-items-center gap-2 fw-lighter">
+                        <font-awesome-icon icon="fa-regular fa-copyright" />
+                        Copyright by Luca Macedone, 2023
+                    </h5>
+                </div>
             </div>
         </div>
     </footer>
