b7d2444b4b49d348735a9d3af6415be6d7904fa3
luca-macedone <luca.macedone@outlook.it>
2023-06-24 16:15:01 +0200

    add new api path

--- src/store.js
+++ src/store.js
@@ -3,11 +3,11 @@ import { reactive } from "vue";
 export const store = reactive({
     base_url: 'http://127.0.0.1:8000/',
     projects_api: 'api/projects/',
+    download_api: 'api/download/',
     latest_projects_api: 'api/latest_projects',
     project_placeholders: 6,
     images_folder: '/src/assets/images/',
     technologies_folder: 'technologies/',
     svg_folder: 'svg/',
     localization_input: true,
-
 });
