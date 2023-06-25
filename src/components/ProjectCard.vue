3c6cb3bc5c4c7c460dccf113b705b914ad29b274
luca-macedone <luca.macedone@outlook.it>
2023-06-25 12:42:57 +0200

    add bg for better readability

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -37,12 +37,14 @@ export default {
                         <div class="card-title fs-4 text-uppercase">{{ data.title }}</div>
                         <div class="d-flex flex-column align-items-start gap-3">
                             <div class="d-flex justify-content-start align-items-center gap-2 flex-wrap">
-                                <span class="badge rounded-pill ms-bg-secondary px-3 py-2 p-2"
+                                <span class="badge rounded-pill ms-bg-light ms-text-dark px-3 py-2 p-2"
                                     v-for="technology in data.technologies">
                                     {{ technology.name }}
                                 </span>
                             </div>
-                            <div :class="description ? '' : 'd-none'">{{ showDescription().substring(0, 100) + ' ...' }}
+                            <div class="ms-text-light description" :class="description ? '' : 'd-none'">{{
+                                showDescription().substring(0,
+                                    100) + ' ...' }}
                             </div>
                         </div>
                     </div>
