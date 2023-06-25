3a2e2f7dc5e19197d7e51243afaa661e3e826774
luca-macedone <luca.macedone@outlook.it>
2023-06-25 12:41:39 +0200

    add brands icons

--- src/main.js
+++ src/main.js
@@ -7,11 +7,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faMagnifyingGlass, faX, faChevronRight, faChevronLeft, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { faCopyright } from '@fortawesome/free-regular-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-library.add(faMagnifyingGlass, faX, faChevronLeft, faChevronRight, faSpinner, faCopyright)
+library.add(faMagnifyingGlass, faX, faChevronLeft, faChevronRight, faSpinner, faCopyright, faLinkedin, faGithub)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
